1331ceae5d0a9b715f1713c2cfbe70ba7d81ec7d
nottus <mcelroy.sc@gmail.com>
2021-03-09 16:55:48 -0800

    done + bonus

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos - 1]) {
+      return this.items[pos];
+    } else {
+      throw new Error('OufOfBounds');
+    }
+  }
+    
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return (Math.max(...this.items));
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return (Math.min(...this.items));
+    }
+  }
 
-  min() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+    return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
